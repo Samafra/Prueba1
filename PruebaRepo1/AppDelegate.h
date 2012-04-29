0dcabffdaf43009e50998a19467ad1724ef32e7f
Francisco <francisco@samafra.com>
2012-04-29 16:47:42 +0200

    Initial Commit

---
+++ PruebaRepo1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PruebaRepo1
+//
+//  Created by Francisco Fernandez Medina on 29/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ PruebaRepo1/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  PruebaRepo1
+//
+//  Created by Francisco Fernandez Medina on 29/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    [pool release];
+    return retVal;
+}
